616e3167789399e1c47e8f65f4c9e3e01df6d4f2
Art Navsegda <artnavsegda@gmail.com>
2020-09-09 19:54:33 +0300

    Update App.js

--- artnavsegda/src/App.js
+++ artnavsegda/src/App.js
@@ -1,31 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  myChangeHandler = (event) => {
-    clearTimeout(this.timerID);
-    this.timerID = setTimeout((myValue)=>{
-      console.log("time out");
-      console.log(myValue);
+function App() {
+  const [timerID, setTimerId] = useState(0);
 
-      fetch("https://artnavsegda.herokuapp.com/q?ask=" + myValue)
-      .then(response => response.json())
-      .then(variants => console.log(variants));
-
-    }, 1000, event.target.value);
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <form>
-            <input type="text" onChange={this.myChangeHandler} />
-          </form>
-          
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form>
+          <input type="text" onChange={(event)={
+            clearTimeout(timerID)
+            setTimerId(setTimeout((askValue) => {
+              fetch("https://artnavsegda.herokuapp.com/q?ask=" + askValue)
+              .then(response => response.json())
+              .then(variants => console.log(variants));
+            }, 1000, event.target.value))
+          }} />
+        </form>
+      </header>
+    </div>
+  );
 }
 
 export default App;
