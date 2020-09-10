c48121da7530791bad804c25010aef3fd598953e
Art Navsegda <artnavsegda@gmail.com>
2020-09-10 10:41:35 +0300

    Update App.js

--- artnavsegda/src/App.js
+++ artnavsegda/src/App.js
@@ -1,22 +1,28 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
-  const [timerID, setTimerId] = useState(0);
-  const [suggestions, setSuggestions] = useState("suggestions");
+  const [timerID, setTimerId] = useState(0)
+  const [suggestions, setSuggestions] = useState("suggestions")
+  const [status, setStatus] = useState("ready")
 
   return (
     <div className="App">
       <header className="App-header">
         <form>
+        <div>{status}</div>
           <input type="text" onChange={(event)=>{
             clearTimeout(timerID)
             setTimerId(setTimeout((askValue) => {
               fetch("https://artnavsegda.herokuapp.com/q?ask=" + askValue)
               .then(response => response.json())
-              .then(variants => setSuggestions(variants.toString()))
+              .then(variants => {
+                setSuggestions(variants.toString())
+                setStatus("loaded")
+              })
             }, 1000, event.target.value))
-          }} /><label>loading</label>
+            setStatus("loading")
+          }} />
         </form>
         <div>{suggestions}</div>
       </header>
