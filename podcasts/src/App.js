e0fba927030ebe88bd33915f9c528478f55e7c03
Art Navsegda <artnavsegda@gmail.com>
2020-10-27 16:01:05 +0300

    index css

--- podcasts/src/App.js
+++ podcasts/src/App.js
@@ -1,5 +1,36 @@
 import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
 
+const initialState = {
+  url: '',
+  loading: false,
+  error: false,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'REQUESTED_DOG':
+      return {
+        url: '',
+        loading: true,
+        error: false,
+      };
+    case 'REQUESTED_DOG_SUCCEEDED':
+      return {
+        url: action.url,
+        loading: false,
+        error: false,
+      };
+    case 'REQUESTED_DOG_FAILED':
+      return {
+        url: '',
+        loading: false,
+        error: true,
+      };
+    default:
+      return state;
+  }
+};
+
 function App() {
   return (
     <div className="App">
