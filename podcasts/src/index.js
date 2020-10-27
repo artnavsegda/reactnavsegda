b32f669114749c9710071a613ef52de5a87de3dc
Art Navsegda <artnavsegda@gmail.com>
2020-10-27 16:08:22 +0300

    meh

--- podcasts/src/index.js
+++ podcasts/src/index.js
@@ -1,17 +1,94 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
 import createSagaMiddleware from 'redux-saga'
+import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
+
+const initialState = {
+  url: '',
+  loading: false,
+  error: false,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'REQUESTED_DOG':
+      return {
+        url: '',
+        loading: true,
+        error: false,
+      };
+    case 'REQUESTED_DOG_SUCCEEDED':
+      return {
+        url: action.url,
+        loading: false,
+        error: false,
+      };
+    case 'REQUESTED_DOG_FAILED':
+      return {
+        url: '',
+        loading: false,
+        error: true,
+      };
+    default:
+      return state
+  }
+}
+
+const requestDog = () => {
+  return { type: 'REQUESTED_DOG' }
+};
+
+const requestDogSuccess = (data) => {
+  return { type: 'REQUESTED_DOG_SUCCEEDED', url: data.message }
+};
+
+const requestDogError = () => {
+  return { type: 'REQUESTED_DOG_FAILED' }
+};
+
+const fetchDog = () => {
+  return { type: 'FETCHED_DOG' }
+};
+
+function* watchFetchDog() {
+  yield takeEvery('FETCHED_DOG', fetchDogAsync)
+}
+
+function* fetchDogAsync() {
+  try {
+    yield put(requestDog());
+    const data = yield call(() => {
+      return fetch('https://dog.ceo/api/breeds/image/random')
+              .then(res => res.json())
+      }
+    )
+    yield put(requestDogSuccess(data));
+  } catch (error) {
+    yield put(requestDogError())
+  }
+}
+
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+)
+
+sagaMiddleware.run(watchFetchDog)
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
     </div>
-  );
+  )
 }
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
